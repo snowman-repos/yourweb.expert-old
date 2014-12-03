19578fd24e31f6c9beed0fce18d5306b301ecd8f
darryl-snow <dazsnow63@yahoo.com>
2014-12-03 16:08:22 +0800

    deploy

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-/* darrylsnow : 0.1.0 : Wed Dec 03 2014 15:43:54 GMT+0800 (CST) */(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* darrylsnow : 0.1.0 : Wed Dec 03 2014 16:07:59 GMT+0800 (CST) */(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Blog;
 
 require("es6-promise").polyfill();
